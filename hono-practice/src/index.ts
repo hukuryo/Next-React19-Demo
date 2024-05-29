1a3a7eafdcf694fb800703f67a58d6642158dea4
hukuryo <ryohei.1998.05.09@gmail.com>
2024-05-29 22:23:40 +0900

    honoでAPIを作成

--- hono-practice/src/index.ts
+++ hono-practice/src/index.ts
@@ -1,9 +1,61 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { prettyJSON } from "hono/pretty-json";
 
-const app = new Hono()
+let blogPosts = [
+  {
+    id: "1",
+    title: "blog1",
+    content: "content1",
+  },
+  {
+    id: "2",
+    title: "blog2",
+    content: "content2",
+  },
+  {
+    id: "3",
+    title: "blog3",
+    content: "content3",
+  },
+];
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const app = new Hono();
+app.use("*", prettyJSON());
 
-export default app
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/entry/:id", (c) => {
+  const id = c.req.param("id");
+  return c.json({
+    "you id is": id,
+  });
+});
+
+app.post("/post", async (c) => {
+  const { title, content } = await c.req.json<{
+    title: string;
+    content: string;
+  }>();
+
+  const newPost = { id: String(blogPosts.length + 1), title, content };
+  blogPosts = [...blogPosts, newPost];
+});
+
+app.put("/post/:id", async (c) => {
+  const id = c.req.param("id");
+  const index = blogPosts.findIndex((p) => p.id === id);
+
+  if (index === -1) {
+    return c.json({ message: "Post not found" }, 400);
+  }
+
+  const { title, content } = await c.req.json();
+
+  blogPosts[index] = { ...blogPosts[index], title, content };
+
+  return c.json(blogPosts[index]);
+});
+
+export default app;
