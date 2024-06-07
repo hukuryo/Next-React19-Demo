a47f4d190970bd9f2e8544cb2014e122313a1ec5
hukuryo <ryohei.1998.05.09@gmail.com>
2024-06-08 08:24:43 +0900

    メソッドを別のコンポーネントに移動

--- app/components/RefReceive.tsx
+++ app/components/RefReceive.tsx
@@ -3,12 +3,5 @@
 import { useCallback } from "react";
 
 export const RefReceive = ({ ref }: { ref: React.RefObject<HTMLDivElement> }) => {
-    const refCallback = useCallback((node: HTMLDivElement) => {
-        console.log('activation', node);
-        return () => {
-        console.log('cleanup', node);
-        };
-    }, []);
-
     return <div ref={ref}>Hello</div>;
 }
